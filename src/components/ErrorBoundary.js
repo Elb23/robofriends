aa850064c449fe54b3370a5a5cbafee317bb640a
Elb23 <127051694+Elb23@users.noreply.github.com>
2023-11-04 18:47:06 +0100

    ts

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -16,7 +16,7 @@ class ErrorBoundary extends Component {
 
     render() {
         if(this.state.hasError){
-            return <h1>Ooops. That is not good</h1>
+            return <h1>NEW ERROR</h1>
         }
         return this.props.children
     }
